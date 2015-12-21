3d97d2f5ca23b7464a72bb09e29c47faa6366f00
Xie Chenhong <chenhong.xie@gmail.com>
2015-12-22 00:35:48 +0800

    UPD: JS event binding done

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,6 +1,84 @@
 'use strict';
 
 $(function() {
+
+    var order_detail = {};
+
+    var gen_dish_payable = function() {
+            var e = this;
+            var name = $(e).find('.dish-name').html(),
+                count = parseInt($(e).find('.dish-count').val()),
+                price = parseFloat(
+                            $(e).find('.price')
+                                .html()
+                                .split('$')[1]
+                                .split('!')[0]
+                        );
+            return {
+                name: name,
+                count: count,
+                price: price * count
+            };
+        },
+
+        gen_dish_summary = function() {
+            var summary = '<p>' +
+                '<span>' +
+                    this.count + ' x ' + this.name +
+                '</span>' +
+                '<span class="pull-right">$' + this.price + '</span>' +
+                '</p>';
+            return summary;
+        },
+
+        gen_all = function(dishes) {
+            var all_summary = dishes.map(gen_dish_summary).get().join(' '),
+                total_sum = dishes.map(function() { return this.price; }).get().reduce(function(a, b) { return a + b; });
+            $('.per-dish').html(all_summary);
+            $('.payable').html('Totoal: $' + total_sum);
+        },
+
+        validate_field = function(obj, name, lo_bound, hi_bound) {
+            return obj[name] && obj[name].length > lo_bound && obj[name].length <= hi_bound;
+        },
+
+        validate_contact = function(contact) {
+            var has_phone = validate_field(contact, 'phone', 0, 8),
+                has_name = validate_field(contact, 'name', 0, 32),
+                has_location = validate_field(contact, 'location', 0, 32),
+                has_zip = validate_field(contact, 'zipcode', 0, 6),
+                has_slot = validate_field(contact, 'slot', 0, 1);
+            return has_phone && has_name &&
+                has_location && has_zip && has_slot;
+        },
+
+        get_contact_detail = function() {
+            var phone = $('#phone'). val(),
+                name = $('#name').val(),
+                zipcode = $('#zip-code').val(),
+                location = $('#location').val(),
+                slot = $('input[name="deliver-time"]:checked').val(),
+                remark = $('#remark').val();
+            var contact = {
+                name: name,
+                zipcode: zipcode,
+                location: location,
+                slot: slot,
+                remark: remark
+            };
+            if (validate_contact(contact)) {
+                order_detail.contact = contact;
+                $.post(
+                    '/test/',
+                    contact,
+                    function() {
+                        $('.contact-filled').html('&#10004;');
+                    }
+                );
+            }
+            return false;
+        };
+
     $('.plus1').on('click', function() {
         var last = parseInt($(this).prev().val()) || 0;
         $(this).prev().val(last + 1);
@@ -14,4 +92,19 @@ $(function() {
         }
         return false;
     });
+
+    $('.order-done').on('click', function() {
+        var dishes = $('.dish-preview')
+            .map(gen_dish_payable)
+            .filter(function() { return this.count > 0; });
+        if (dishes.length > 0) {
+            $('#order-choice').hide();
+            $('#contact-detail').show();
+            order_detail.dishes = dishes;
+            gen_all(dishes);
+        }
+        return false;
+    });
+
+    $('.contact-filled').on('click', get_contact_detail);
 })
