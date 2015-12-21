b0df5245afc2dd5b4fcb598dafbd8e07456973b6
Xie Chenhong <chenhong.xie@gmail.com>
2015-12-21 08:46:01 +0800

    UPD: binding plus and minus of dish

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -1,1 +1,17 @@
+'use strict';
 
+$(function() {
+    $('.plus1').on('click', function() {
+        var last = parseInt($(this).prev().val()) || 0;
+        $(this).prev().val(last + 1);
+        return false;
+    });
+
+    $('.minus1').on('click', function() {
+        var last = parseInt($(this).next().val()) || 0;
+        if (last > 0) {
+            $(this).next().val(last - 1);
+        }
+        return false;
+    });
+})
