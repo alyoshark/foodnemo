b6b668e83459bf2d6579906ee2a6d09337374b83
Xie Chenhong <xiech@garena.com>
2016-01-05 00:09:48 +0800

    UPD: date suffix - from FreeBSD to Linux style

--- sa/rotate.sh
+++ sa/rotate.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../raw
 
-date_suffix=`date -v-1d +'%Y%m%d'`
+# date_suffix=`date -v-1d +'%Y%m%d'`  # FreeBSD Style
+date_suffix=`date +'%Y%m%d' -d'Yesterday'`  # Linux Style
 
 test_rot=test-order.json-$date_suffix
 mv test-order.json $test_rot
